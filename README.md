de7b841b2b85f70dcb65cc3f9186cbda2c9d5cfb
Dale Newman <dalenewman@gmail.com>
2018-01-22 11:43:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Transformalize.Transform.PoorMansTSqlFormatter
---
+++ src/Transformalize.Transform.PoorMansTSqlFormatter/FormatSqlTransform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using PoorMansTSqlFormatterLib;
+using Transformalize.Configuration;
+using Transformalize.Contracts;
+
+namespace Transformalize.Transforms.PoorMansTSqlFormatter {
+    public class FormatSqlTransform : StringTransform {
+        private readonly Field _input;
+        private readonly SqlFormattingManager _formattingManager;
+
+        public FormatSqlTransform(IContext context = null) : base(context, "string") {
+            if (IsMissingContext()) {
+                return;
+            }
+            _input = SingleInput();
+            _formattingManager = new SqlFormattingManager();
+        }
+
+        public override IRow Operate(IRow row) {
+            var unformatted = GetString(row, _input);
+            row[Context.Field] = _formattingManager.Format(unformatted);
+            Increment();
+            return row;
+        }
+
+        public override IEnumerable<OperationSignature> GetSignatures() {
+            return new[] { new OperationSignature("formatsql") };
+        }
+    }
+}
